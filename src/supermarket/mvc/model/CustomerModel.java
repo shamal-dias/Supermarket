01d59feb1d0be2874762c15fa5c239a23d895e60
Shamal <dias.mlsp@gmail.com>
2023-07-30 11:54:55 +0530

    toString removed

--- src/supermarket/mvc/model/CustomerModel.java
+++ src/supermarket/mvc/model/CustomerModel.java
@@ -9,5 +9,104 @@ package supermarket.mvc.model;
  * @author Dell
  */
 public class CustomerModel {
+    private String custId;
+    private String title;
+    private String name;
+    private String dob;
+    private Double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String zip;
+
+    public CustomerModel() {
+    }
+
+    public CustomerModel(String custId, String title, String name, String dob, Double salary, String address, String city, String province, String zip) {
+        this.custId = custId;
+        this.title = title;
+        this.name = name;
+        this.dob = dob;
+        this.salary = salary;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.zip = zip;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    
+
+    
     
 }
